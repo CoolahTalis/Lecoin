4839da2a34ffdf40a5219e0ffeb19e22c2db77ac
Co0lah <celes.thym@laposte.net>
2020-11-03 16:03:29 +0100

    last commit 03/11/20

--- includes/functions.php
+++ includes/functions.php
@@ -2,8 +2,10 @@
 
 require 'includes/config.php';
 
-function inscription($email, $username, $password1, $password2, $conn)
+function inscription($email, $username, $password1, $password2)
 {
+    global $conn;
+
     try {
         $sql1 = "SELECT * FROM users WHERE email = '{$email}'";
         $sql2 = "SELECT * FROM users WHERE username = '{$username}'";
@@ -21,17 +23,70 @@ function inscription($email, $username, $password1, $password2, $conn)
                     $sth->bindValue(':username', $username);
                     $sth->bindValue(':password', $password1);
                     $sth->execute();
-                    echo "L'utilisateur a bien été enregistré";
+                    echo '<div class="alert alert-success mt-2" role="alert" > L\'utilisateur a bien été enregistré </div>';
                 } else {
-                    echo 'Les mots de passes ne concordent pas !';
+                    echo '<div class="alert alert-danger mt-2" role="alert "> Les mots de passes ne concordent pas ! </div>';
                 }
             } elseif ($count_user > 0) {
-                echo "Ce nom d'utilisateur est déja pris !";
+                echo '<div class="alert alert-danger mt-2" role="alert" > Ce nom d\'utilisateur est déja pris ! </div>';
             }
         } elseif ($count_email > 0) {
-            echo 'Cette adresse mail existe déjà !';
+            echo '<div class="alert alert-danger mt-2" role="alert"> Cette adresse mail existe déjà ! </div>';
+            unset($_POST);
         }
     } catch (PDOException $e) {
         echo 'Error: '.$e->getMessage();
+    }
+}
+
+function connexion($conn, $email, $password)
+{
+    global $conn;
+
+    try {
+        $sql = "SELECT * FROM users WHERE email = '{$email}'";
+        $res = $conn->query($sql);
+        $user = $res->fetch(PDO::FETCH_ASSOC);
+        if ($user) {
+            $db_password = $user['password'];
+            if (password_verify($password, $db_password)) {
+                $_SESSION['id'] = $user['id'];
+                $_SESSION['email'] = $user['email'];
+                $_SESSION['username'] = $user['username'];
+
+                echo '<div class="alert alert-success mt-2" role="alert" > Vous êtes désormais connecté ! </div>';
+                header('Location: index.php');
+            } else {
+                echo '<div class="alert alert-danger mt-2" role="alert" > Le mot de passe est erroné ! </div>';
+                unset($_POST);
+            }
+        } else {
+            echo '<div class="alert alert-danger mt-2" role="alert" > L\'email utilisé n\'est pas reconnu ! </div>';
+        }
+    } catch (PDOException $e) {
+        echo 'Error: '.$e->getMessage();
+    }
+}
+
+function affichage()
+{
+    global $conn;
+
+    $sth = $conn->prepare('SELECT * FROM users');
+    $sth->execute();
+    $users = $sth->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($users as $user) {
+        ?>
+<tr>
+    <th scope="row"><?php echo $user['id']; ?>
+    </th>
+    <td><?php echo $user['email']; ?>
+    </td>
+    <td><?php echo $user['username']; ?>
+    </td>
+    <td><?php echo $user['password']; ?>
+    </td>
+</tr>
+<?php
     }
 }
--- signin.php
+++ signin.php
@@ -1,8 +1,8 @@
 <?php
 
 $title = 'Accueil - Le Chouette Coin';
+// Function doit $etre en amont ..
 require 'includes/header.php';
-require 'includes/functions.php';
 
 if (!empty($_POST['email_signup']) && !empty($_POST['password1_signup']) && !empty($_POST['username_signup'])
     && isset($_POST['submit_signup'])) {
@@ -11,17 +11,28 @@ if (!empty($_POST['email_signup']) && !empty($_POST['password1_signup']) && !emp
     $password2 = htmlspecialchars($_POST['password2_signup']);
     $username = htmlspecialchars($_POST['username_signup']);
 
-    inscription($email, $username, $password1, $password2, $conn);
+    inscription($email, $username, $password1, $password2);
+} elseif (!empty($_POST['email_login']) && !empty($_POST['password_login']) && isset($_POST['submit_login'])) {
+    $email = strip_tags($_POST['email_login']);
+    $password = strip_tags($_POST['password_login']);
+
+    connexion($conn, $email, $password);
+} else {
+    if (isset($_POST)) {
+        unset($_POST);
+    }
 }
 ?>
 
 <div class="row">
     <div class="col-6">
+        <h3>S'inscrire</h3>
+
         <form
             action="<?php $_SERVER['REQUEST_URI']; ?>"
             method="POST">
             <div class="form-group">
-                <label for="InputEmail1"> Addresse mail</label>
+                <label for="InputEmail1"> Adresse mail</label>
                 <input type="email" class="form-control" aria-describedby="emailHelp" id="InputEmail1"
                     name="email_signup" required>
                 <small id="emailHelp" class="form-text text-muted">Nous ne partageons jamais votre email avec qui que ce
@@ -50,23 +61,20 @@ if (!empty($_POST['email_signup']) && !empty($_POST['password1_signup']) && !emp
         </form>
     </div>
     <div class="col-6">
-        <form>
+        <h3>Se Connecter</h3>
+        <form
+            action="<?php $_SERVER['REQUEST_URI']; ?>"
+            method="POST">
             <div class="form-group">
-                <label">Adresse mail</label>
-                    <input type="email" class="form-control" aria-describedby="emailHelp">
-                    <small id="emailHelp" class="form-text text-muted">Nous ne partageons jamais votre email avec qui
-                        que ce
-                        soit.</small>
+                <label for="InputEmail2">Adresse mail</label>
+                <input type="email" class="form-control" aria-describedby="emailHelp" id="InputEmail2"
+                    name="email_login" required>
             </div>
             <div class="form-group">
-                <label for="InputUsername1">Mot de passe</label>
-                <input type="password" class="form-control" id="InputUsername1">
+                <label for="InputPassword">Mot de passe</label>
+                <input type="password" class="form-control" id="InputPassword" name="password_login" required>
             </div>
-            <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Se connecter</button>
+            <button type="submit" class="btn btn-primary" name="submit_login" value="connexion">Se connecter</button>
         </form>
     </div>
 </div>
